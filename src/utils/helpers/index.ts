b6a6b7247aa59df56806e0b60fee764569205fff
gizeasy <gizeasy@gmail.com>
2021-06-28 15:16:01 +0300

    feat(browserCompatibility): refactor
    issue -  https://github.com/gazprom-neft/consta-uikit/issues/1156

--- src/utils/helpers/index.ts
+++ src/utils/helpers/index.ts
@@ -1,33 +1,10 @@
 import { browserName, browserVersion } from 'react-device-detect';
 
 export const getBrowserCompatibility = () => {
-  let isCompatible = false;
-  switch (browserName) {
-    case 'Chrome':
-      if (Number(browserVersion) >= 79) {
-        isCompatible = true;
-      }
-      break;
-    case 'Edge':
-      if (Number(browserVersion) >= 79) {
-        isCompatible = true;
-      }
-      break;
-    case 'Firefox':
-      if (Number(browserVersion) >= 75) {
-        isCompatible = true;
-      }
-      break;
-    case 'Opera':
-      if (Number(browserVersion) >= 66) {
-        isCompatible = true;
-      }
-      break;
-    case 'Safari':
-      if (Number(browserVersion) >= 13) {
-        isCompatible = true;
-      }
-      break;
+  let isCompatible = true;
+
+  if (browserName === 'IE') {
+    isCompatible = false;
   }
 
   return {
