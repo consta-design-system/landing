7b658d3297dccbbf9d7d044324011dbc835c9fdf
gizeasy <gizeasy@gmail.com>
2021-06-28 17:31:01 +0300

    dd

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import { connectReduxDevtools } from '@reatom/debug';
 import { context } from '@reatom/react';
 
 import { App } from './containers/App/App';
@@ -9,7 +8,6 @@ import { store } from '@/modules/app';
 import { MainPage } from '@/pages/MainPage/MainPage';
 
 const Root: React.FC = () => {
-  useEffect(() => connectReduxDevtools(store), []);
   return (
     <context.Provider value={store}>
       <App>
