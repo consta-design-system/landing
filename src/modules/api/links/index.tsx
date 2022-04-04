b4c22bbd8b56ed75cb4554b7462cc60c2e58c3c2
gizeasy <gizeasy@gmail.com>
2022-04-04 13:59:32 +0300

    feat(Links): edit links (#24)

--- src/modules/api/links/index.tsx
+++ src/modules/api/links/index.tsx
@@ -1,5 +1,5 @@
 export const licenceMit = '/static/licence_mit.pdf';
-export const constaUikit = 'https://consta-uikit.vercel.app';
+export const constaUikit = 'http://uikit.gizeasy.ru';
 export const constaUikitDevelopContributors = `${constaUikit}/?path=/docs/common-develop-contributors--page`;
 export const contsaCommunityFigma = 'https://www.figma.com/@consta';
 export const constaFigma = 'https://www.figma.com/community/file/853774806786762374';
@@ -7,7 +7,7 @@ export const constaGitHub = 'https://github.com/consta-design-system/uikit';
 export const constaNPM = 'https://www.npmjs.com/package/@consta/uikit';
 export const constaTelegram = 'https://t.me/consta_ui_releases';
 
-export const constaCharts = 'http://consta-charts.vercel.app/';
+export const constaCharts = 'http://charts.gizeasy.ru/';
 export const constaChartsFigma =
   'https://www.figma.com/community/file/982611119114314434/Consta-Charts';
 export const constaChartsGitHub = 'https://github.com/consta-design-system/charts';
